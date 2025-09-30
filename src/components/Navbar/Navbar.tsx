f5d525618aa48161605c266c8e1ab8d4dcbded9d
Chris <christophermedinabarroso@gmail.com>
2025-09-30 10:56:56 -0400

    primeros componentes

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Navbar: React.FC = () => {
+    return (
+        <>
+            <nav className='w-full'>
+                <div>
+                    <img src="" alt="" />
+                    <span>Example</span>
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default Navbar;
